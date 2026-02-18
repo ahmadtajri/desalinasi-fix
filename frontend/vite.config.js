6d5de5c9086430c6c7704223813883bfecae4221
ahmadtajri <tajriahmad024@gmail.com>
2026-02-18 21:22:39 +0700

    Remove HMR configuration from Vite server settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -40,10 +40,5 @@ export default defineConfig({
     server: {
         host: true,
         port: 5173,
-        hmr: {
-            protocol: 'ws',
-            host: 'localhost',
-            port: 5173,
-        }
     },
 })
